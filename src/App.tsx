8197e67484e9b415a1415b9366a68272c01c37c9
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-01-15 01:20:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+	return (
+		<div className="App" >
+			<h1>Daniel's Portfolio page</h1>
+			<div>
+				<h2>Home / About me</h2>
+				<h2>Projects / Technology / Skills</h2>
+				<h2>Education</h2>
+				<h2>Experience</h2>
+				<h2>Contact</h2>
+			</div>
+		</div>
+	)
+}
+
+export default App
