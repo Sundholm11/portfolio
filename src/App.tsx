a88b10257cba487b7a8b2a40dc8422414470bd6a
Daniel Sundholm <dajsun@utu.fi>
2020-01-15 19:47:42 +0200

    Projects rough initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './styles/App.css'
 
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 
 const App: React.FC = () => {
 	return (
 		<div className="App" >
 			<Navbar />
 			<Home />
+			<Projects />
 		</div>
 	)
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import '../styles/App.css'
+
+const Projects: React.FC = () => {
+    return (
+        <div>
+            <div className="subContainer2">
+                <h2>Projects</h2>
+            </div>
+            <div className="subContainer2">
+                <h2>Another Column</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
