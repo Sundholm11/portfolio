b02e569d6ab4c0a43946efcc7692eeff01bfb810
Daniel Sundholm <dajsun@utu.fi>
2020-01-17 16:18:37 +0200

    Adding and handling of projects easier now

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import '../styles/Projects.css'
 
-import Background from '../images/test.JPG'
+import chatappBackground from '../images/test.JPG'
+
+interface Project {
+	id: number,
+	name: string,
+	bgImg: string
+}
 
 const Projects: React.FC = () => {
-	const[projects, setProjects] = useState<Array<Object>>([])
-	const[focusedProject, setFocusedProject] = useState<Object>()
+	const[projects, setProjects] = useState<Array<Project>>([])
+	const[focusedProject, setFocusedProject] = useState<Project>()
+
+	useEffect(() => {
+		initProjects()
+	}, [])
 
 	const handleProjectChange = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
 		setFocusedProject(projects[event.currentTarget.value])
+	}
+
+	const initProjects = () => {
+		const initialProjects = [
+			{ id: 0, name: "Chat-app", bgImg: chatappBackground },
+			{ id: 1, name: "Fullstackopen 2019", bgImg: chatappBackground},
+			{ id: 2, name: "Ilmot-plaseeraus", bgImg: chatappBackground}
+		]
+		setFocusedProject(initialProjects[0])
+		setProjects(projects.concat(initialProjects))
 	}
 
 	return (
@@ -21,14 +41,18 @@ const Projects: React.FC = () => {
 						day and night, to create
 					</p>
 					<hr className="divider" />
-					<li className="projectsVertNav"
-						value={0}
-						onClick={handleProjectChange}>Chat-app
-					</li>
+					{projects.map( (project: any) =>
+					<li key={project.id}
+						className="projectsVertNav"
+						value={project.id}
+						onClick={handleProjectChange}>{project.name}
+					</li>)}
 				</ul>
 			</div>
-			<div className="projectsRightContainer" style={{ backgroundImage: `url(${Background})` }}>
-			</div>
+			{focusedProject !== undefined ? 
+			<div className="projectsRightContainer" style={{ backgroundImage: `url(${focusedProject.bgImg})` }}>
+			</div> :
+			null}
 		</div>
 	)
 }
