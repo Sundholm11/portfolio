4173ae79aa1a2f3993663893c9bbc2956dd020be
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-01-19 19:45:42 +0200

    Navbar scroll-to implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import '../styles/Navbar.css'
 
 const Navbar: React.FC = () => {
+	const scrollWindow = (height: number) => {
+		window.scrollTo(0, height)
+	}
+
 	return (
 		<ul className="navBar">
-			<li className="navBar"><p>Home / About me</p></li>
-			<li className="navBar"><p>Projects / Technology / Skills</p></li>
+			<li className="navBar" onClick={() => scrollWindow(0)}><p>Home / About me</p></li>
+			<li className="navBar" onClick={() => scrollWindow(700)}><p>Projects / Technology / Skills</p></li>
 			<li className="navBar"><p>Education</p></li>
 			<li className="navBar"><p>Experience</p></li>
 			<li className="navBar"><p>Contact</p></li>
