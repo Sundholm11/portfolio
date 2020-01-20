97c6e36d0ab510281656cde9eeb01d985b70c7f7
Daniel Sundholm <dajsun@utu.fi>
2020-01-20 15:05:53 +0200

    Sticky navbar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC = () => {
 	return (
 		<ul className="navBar">
 			<li className="navBar" onClick={() => scrollWindow(0)}><p>Home / About me</p></li>
-			<li className="navBar" onClick={() => scrollWindow(700)}><p>Projects / Technology / Skills</p></li>
+			<li className="navBar" onClick={() => scrollWindow(625)}><p>Projects / Technology / Skills</p></li>
 			<li className="navBar"><p>Education</p></li>
 			<li className="navBar"><p>Experience</p></li>
 			<li className="navBar"><p>Contact</p></li>
