ef088d2f2955a9af2d1ac44a6ed57a31053fca33
Daniel Sundholm <dajsun@utu.fi>
2020-01-22 15:49:09 +0200

    Doodling aboutme styling

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,10 +6,11 @@ const AboutMe: React.FC = () => {
 	return (
 		<div className="mainContainer" style={{ backgroundColor: "#f8f8f8" }}>
 			<div className="aboutLeftContainer">
-                <h2>Pic pic pic</h2>
+                <h2 className="about">Pic pic pic</h2>
             </div>
             <div className="aboutRightContainer">
-                <h2>About me</h2>
+                <h2 className="about">About me</h2>
+                <p className="aboutDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
 		</div>
 	)
