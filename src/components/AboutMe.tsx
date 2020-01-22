a046409cd1e7e89adf14e36888722be7463a0844
Daniel Sundholm <dajsun@utu.fi>
2020-01-22 17:21:46 +0200

    Adjusting aboutme styling

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,11 +6,13 @@ const AboutMe: React.FC = () => {
 	return (
 		<div className="mainContainer" style={{ backgroundColor: "#f8f8f8" }}>
 			<div className="aboutLeftContainer">
-                <h2 className="about">Pic pic pic</h2>
+                <img />
             </div>
             <div className="aboutRightContainer">
-                <h2 className="about">About me</h2>
-                <p className="aboutDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div>
+                    <h2 className="about">About me</h2>
+                    <p className="aboutDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </div>
 		</div>
 	)
