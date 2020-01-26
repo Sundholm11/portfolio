c2d710a126885a889470a30c195e98e12ab5ceac
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-01-26 23:49:13 +0200

    About me text written

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -21,7 +21,24 @@ const AboutMe: React.FC = () => {
             <div className="aboutRightContainer">
                 <div>
                     <h2 className="about">About me</h2>
-                    <p className="aboutDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p className="aboutDescription">
+                        I'm a 21-years old lovely fella from Turku. Three years ago I graduated from the Forssa Upper Secondary school, 
+                        after which I spent the next year doing my military service. Right now I'm studying Computer Science at the University of Turku 
+                        for my second semester. During my free time, I enjoy hanging out with my good friends and doing all kinds of sports. But also just 
+                        having that peaceful evening at home, watching Netflix or browsing the web, is pleasant.
+                    </p>
+                    <p className="aboutDescription">
+                        I first got into programming at upper secondary school. I signed up to a couple of week lasting programming courses organized by Mehackit, 
+                        whom were visiting by our school. Tasked to create a visual java program on the other course, and a robot by using Arduino on the other, 
+                        the practical aspect of programming really inspired me. To create and see your programs come alive, and to see them change, with only few written lines of code 
+                        by you, was really fascinating.
+                    </p>
+                    <p className="aboutDescription">
+                        Nowadays, I'm more or less busy with courses from my studies. Some of them being very interesting, such as learning more tips and tricks for 
+                        object oriented programming or threading and sockets. Some of them being not that much... eh, really wish I'd be done with my swedish course. But one 
+                        thing I'm happy about, I've learned to program in a very diverse field of stuff. Say, I've done a couple of projects for courses coding GUIs, using 
+                        Java and Python, but also during my free time I've had a change to practice web development using JS React.
+                    </p>
                 </div>
             </div>
 		</div>
