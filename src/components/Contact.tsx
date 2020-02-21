e7faf2d2b8ac6a1cd904c9b60492a47f5faf31ab
Daniel Sundholm <daniel.j.sundholm@utu.fi>
2020-02-21 22:05:01 +0200

    Fixes to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,12 +4,16 @@ import '../styles/Contact.css'
 
 const Contact: React.FC = () => {
 	return (
-		<div id="contact" className="mainContainer" style={{ backgroundColor: "#f8f8f8" }}>
-            <h2 className="contact">Contact</h2>
+		<div className="mainContainer" style={{ backgroundColor: "#f8f8f8" }}>
+            <h2 id="contact" className="contact">Contact</h2>
             <hr className="contact"/>
             <table className="contact">
-                <th><h3 className="contact">Email:</h3></th>
-                <th><h3 className="contact">daniel.sundholm@hotmail.fi</h3></th>
+                <tbody>
+                    <tr>
+                        <th><h3 className="contact">Email:</h3></th>
+                        <th><h3 className="contact">daniel.sundholm@hotmail.fi</h3></th>
+                    </tr>
+                </tbody>
             </table>
 		</div>
 	)
