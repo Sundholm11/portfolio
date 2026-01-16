d9b963a6a67e007abf16ba6285be310cabc2be5c
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2026-01-16 19:22:31 +0200

    Setup github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   plugins: [
     react()
   ],
+  base: command === "build" ? "/portfolio/" : "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
